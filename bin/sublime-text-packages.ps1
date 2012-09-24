2248565b3ac01080f17d9f1c1934810ba65b698e
Ryan Seto <mr.werewolf@gmail.com>
2012-09-24 10:34:40 -0400

    Use Sublime Package Control to install most packages for us.

--- bin/sublime-text-packages.ps1
+++ bin/sublime-text-packages.ps1
@@ -21,20 +21,3 @@ $packages = Resolve-Path "$env:UserProfile\AppData\Roaming\Sublime Text 2\Packag
 # https://github.com/SublimeText/VintageEx
 $projName = "VintageEx"
 GitClone "git://github.com/SublimeText/$projName.git" "$packages\$projName"
-
-# Sass syntax highlighting
-# https://github.com/n00ge/sublime-text-haml-sass
-$projName = "sublime-text-haml-sass"
-GitClone "git://github.com/n00ge/$projName.git" "$packages\$projName"
-SymDirLink "$packages\SASS" "$packages\$projName\SASS"
-SymDirLink "$packages\Ruby Haml" "$packages\$projName\Ruby Haml"
-
-# Adds TextMate-like autocompletion to Sublime Text 2
-# https://github.com/alexstaubo/sublime_text_alternative_autocompletion
-$projName = "sublime_text_alternative_autocompletion"
-GitClone "git://github.com/alexstaubo/$projName.git" "$packages\$projName"
-
-# Sublime Text 2 plugin for running ruby tests! (Unit, RSpec, Cucumber)
-# https://github.com/maltize/sublime-text-2-ruby-tests
-$projName = "sublime-text-2-ruby-tests"
-GitClone "git://github.com/maltize/$projName.git" "$packages\RubyTest"
