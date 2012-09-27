7376cfd5f21952e4150ee5d6c2d6af58f3f928ce
Ryan Seto <mr.werewolf@gmail.com>
2012-09-27 08:00:05 -0400

    Add kdiff3rc to dotfiles

--- bin/Setup.ps1
+++ bin/Setup.ps1
@@ -15,6 +15,8 @@ Function Get-ScriptDirectory
 $dir = Get-ScriptDirectory
 # dotfiles dir
 $root = Resolve-Path "$dir\.."
+# Shortcut to the User's home folder
+$userHome = $env:UserProfile
 
 # Import Libraries
 . "$root\lib\PowerShell\SymLink.ps1"
@@ -23,14 +25,14 @@ echo "Linking: PowerShell Profile"
 SymLink $profile $root\Microsoft.PowerShell_profile.ps1
 
 echo "Linking: Vim Config"
-SymLink $env:UserProfile\.vimrc $root\vimrc
-SymDirLink $env:UserProfile\vimfiles $root\vim
+SymLink $userHome\.vimrc $root\vimrc
+SymDirLink $userHome\vimfiles $root\vim
 
 echo "Linking: Mercurial Config"
-SymLink $env:UserProfile\mercurial.ini $root\hgrc
+SymLink $userHome\mercurial.ini $root\hgrc
 
 echo "Linking: Ruby Gems Config"
-SymLink $env:UserProfile\.gemrc $root\gemrc
+SymLink $userHome\.gemrc $root\gemrc
 
 # Setup SublimeText configuration.
 . "$dir\SublimeText.ps1"
@@ -39,4 +41,7 @@ echo "Linking: ConEmu Config"
 SymLink "C:\Program Files\ConEmu\ConEmu.xml" $root\ConEmu.xml
 
 echo "Linking: Mercurial Extensions"
-SymDirLink $env:UserProfile\.hgext $root\hgext
+SymDirLink $userHome\.hgext $root\hgext
+
+echo "Linking: kdiff3 Config"
+SymLink $userHome\.kdiff3rc $root\kdiff3rc
