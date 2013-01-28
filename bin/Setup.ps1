4d590462daf9746e332894249edff0b80c4bd39a
Ryan Seto <mr.werewolf@gmail.com>
2013-01-28 09:24:34 -0500

    Remove PuTTY Tray since we're using KiTTY.

--- bin/Setup.ps1
+++ bin/Setup.ps1
@@ -70,10 +70,6 @@ SymLink -dir $dot\hgext      $userHome\.hgext
 echo "Linking: kdiff3 Config"
 SymLink      $dot\kdiff3rc   $userHome\.kdiff3rc
 
-Write-Host "Linking: PuTTY Tray Config"
-SymLink -dir $dot\PuTTY\sessions  $userHome\tools\PuTTY\AMD64\sessions
-SymLink -dir $dot\PuTTY\sessions  $userHome\tools\PuTTY\x86\sessions
-
 Write-Host "Linking: Pentadactyl Firefox Addon"
 SymLink -dir $dot\pentadactyl               $userHome\pentadactyl
 SymLink      $dot\pentadactyl\pentadactylrc $userHome\_pentadactylrc
