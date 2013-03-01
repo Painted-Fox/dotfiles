5cee1dc16d5e10673afb861d19b9bea5e439b5db
Ryan Seto <mr.werewolf@gmail.com>
2013-03-01 09:16:20 -0500

    emacs: Setup emacs to automatically install selected packages.

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,19 @@
+;; Color theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;;(load-theme 'solarized-dark t)
+(load-theme 'zenburn t)
+
+;; UTF-8
+(prefer-coding-system 'utf-8)
+
+;; Default font
+(custom-set-faces
+ '(default ((t (:family "Consolas for Powerline FixedD" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+
+;; =============================================================================
+;; Package Management
+;; =============================================================================
+
 ;; Setup the Marmalade package repository.
 (require 'package)
 (add-to-list 'package-archives
@@ -5,19 +21,19 @@
       "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;; Ensure my packages are installed.
+;; Solution copied from: https://github.com/technomancy/emacs-starter-kit
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(
+  evil)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; Load Evil
 (require 'evil)
 (evil-mode 1)
-
-;; Color theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;;(load-theme 'solarized-dark t)
-(load-theme 'zenburn t)
-
-
-;; Default font
-(custom-set-faces
- '(default ((t (:family "Consolas for Powerline FixedD" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
-
-;; UTF-8
-(prefer-coding-system 'utf-8)
