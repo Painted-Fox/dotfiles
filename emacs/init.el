21ab4e6f73470aea0a7a36c59ead8e3c50348aed
Ryan Seto <mr.werewolf@gmail.com>
2013-03-08 11:39:10 -0500

    Config whitespace-mode.  Tabs are 2 char wide.  Auto delete trailing whitespace.

--- emacs/init.el
+++ emacs/init.el
@@ -13,8 +13,26 @@
 ;; Default 80 columns wide
 (setq-default fill-column 80)
 
-;; Show 81 columns to make room for the Fill Column Indicator
-(add-to-list 'default-frame-alist '(width . 81))
+;; Show 82 columns to make room for the Fill Column Indicator
+(add-to-list 'default-frame-alist '(width . 82))
+
+;; Default to 2 char wide tabs.
+(setq default-tab-width 2)
+
+;; whitespace-mode
+;; make whitespace-mode use just basic coloring
+(setq whitespace-style (
+  quote (spaces tabs newline space-mark tab-mark newline-mark)))
+(setq whitespace-display-mappings
+ ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
+ '(
+ (space-mark 32 [183] [46]) ; 32 Space 「 」, 183 Middle Dot, 46 Full Stop
+ (newline-mark 10 [8629 10]) ; 10 Line Feed, 8629 Downwards Arrow w/ Corner
+ (tab-mark 9 [8594 9] [92 9]) ; 9 TAB, 8594 Rightwards Arrow, 92 Backslash \
+ ))
+
+;; Delete trailing whitespace before we save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Use aspell for spell check
 (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")
