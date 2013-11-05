062450fc27d5faa1b76f4d037f25730d03bd8d04
Ryan Seto <ryanseto@yak.net>
2013-11-05 11:16:26 -0500

    Don't link mintty or bash on Windows anymore.

--- bin/win.bat
+++ bin/win.bat
@@ -26,10 +26,3 @@ mklink /d "%HOME%\.vim"  "%DOT%\vim"
 mklink "%HOME%\.vimrc"   "%DOT%\vim\vimrc"
 mklink "%HOME%\.gvimrc"  "%DOT%\vim\gvimrc"
 mklink "%HOME%\.vsvimrc" "%DOT%\vim\vsvimrc"
-
-echo "Linking Mintty."
-mklink "%HOME%\.minttyrc" "%DOT%\minttyrc"
-
-echo "Linking Bash"
-mklink "%HOME%\.profile" "%DOT%\profile"
-mklink "%HOME%\.bashrc" "%DOT%\bashrc"
