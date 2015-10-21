3050c34e100ec94d30d9f98da85c31b7268994cd
Ryan Seto <mr.werewolf@gmail.com>
2015-10-21 10:45:34 -0400

    Shortcuts for editing new vim config files.

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,14 @@ set -g -x fish_greeting ''
 
 # Shortcuts to edit config files
 function dot; pushd ~/lib/dotfiles; end
-function eb;  dot; vim bin/bootstrap;    popd; end
-function ef;  dot; vim fish/config.fish; popd; end
-function eg;  dot; vim git/gitconfig;    popd; end
-function eh;  dot; vim mercurial/hgrc;   popd; end
-function et;  dot; vim tmux.conf;        popd; end
-function ev;  dot; vim vim/vimrc;        popd; end
+function eb;  dot; vim bin/bootstrap;           popd; end
+function ef;  dot; vim fish/config.fish;        popd; end
+function eg;  dot; vim git/gitconfig;           popd; end
+function eh;  dot; vim mercurial/hgrc;          popd; end
+function et;  dot; vim tmux.conf;               popd; end
+function ev;  dot; vim vim/vimrc;               popd; end
+function evs; dot; vim vim/vsvimrc;             popd; end
+function evp; dot; vim vim/plugins.vim;         popd; end
+function evc; dot; vim vim/plugins.config.vim;  popd; end
 
 source ~/lib/dotfiles/fish/mark.fish
