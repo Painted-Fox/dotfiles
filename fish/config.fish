ddf48e6f44917f1151f3adc121d1bcdadaf21b64
Ryan Seto <mr.werewolf@gmail.com>
2015-10-23 13:02:42 -0400

    Add alias for tf.

--- fish/config.fish
+++ fish/config.fish
@@ -13,4 +13,7 @@ function evs; dot; vim vim/vsvimrc;             popd; end
 function evp; dot; vim vim/plugins.vim;         popd; end
 function evc; dot; vim vim/plugins.config.vim;  popd; end
 
+# Don't autoconnect on start
+alias tf "tf -n"
+
 source ~/lib/dotfiles/fish/mark.fish
