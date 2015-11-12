ab1417f1be4d40525f0f29e9e8eeda0fda32ff5c
Ryan Seto <mr.werewolf@gmail.com>
2015-11-12 09:33:39 -0500

    Use polipo from fish and test if polipo is running, not just existing.

--- oh-my-fish/init.fish
+++ oh-my-fish/init.fish
@@ -1,3 +1,5 @@
+# Aliases / Shortcuts --------------------------------------------------------
+
 # Shortcuts to edit config files
 function dot; pushd ~/lib/dotfiles; end
 function eb;  dot; vim bin/bootstrap;           popd; end
@@ -13,8 +15,18 @@ function evc; dot; vim vim/plugins.config.vim;  popd; end
 # Don't autoconnect on start
 alias tf "tf -n"
 
+
+# Environment Variables ------------------------------------------------------
+
 # Install oh-my-fish under ~/lib/omf
 set -x OMF_PATH $HOME/lib/omf
 
 # Set vim as my default editor
 set -x EDITOR vim
+
+# If polipo is running, use it.
+# This speeds up w3m, since it has not cache of its own.
+if pgrep polipo > /dev/null
+    set -x HTTP_PROXY http://127.0.0.1:8123/
+end
+
